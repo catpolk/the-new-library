83ff75050a8ba0881aabbff37bca759761bb96aa
Catherine Polk <cpolknytx@gmail.com>
2023-02-01 19:02:26 -0600

    adedd middleware

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
 const { ApolloServer } = require("apollo-server-express");
-const { typeDefs , resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
-
+const path = require('path');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -20,6 +18,7 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
